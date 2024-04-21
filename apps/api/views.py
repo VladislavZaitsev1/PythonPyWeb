fe475c26f07e1b94dc2d83f569950e52bf85afe4
VladislavZaitsev1 <zenit2597@mail.ru>
2024-04-21 11:40:38 +0300

    laba 5

--- apps/api/views.py
+++ apps/api/views.py
@@ -13,6 +13,13 @@ from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
+from rest_framework import permissions
+from rest_framework import authentication
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+
+class AuthorAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
 
 class AuthorPagination(PageNumberPagination):
     page_size = 5  # количество объектов на странице
@@ -101,12 +108,38 @@ class AuthorAPIView(APIView):
         author.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+class CustomPermission(permissions.BasePermission):
+        """
+        Пользователи могут выполнять различные действия в зависимости от их роли.
+        """
+
+        def has_permission(self, request, view):
+            # Разрешаем только GET запросы для неаутентифицированных пользователей
+            if request.method == 'GET' and not request.user.is_authenticated:
+                return True
+
+            # Разрешаем GET и POST запросы для аутентифицированных пользователей
+            if request.method in ['GET', 'POST'] and request.user.is_authenticated:
+                return True
+
+            # Разрешаем все действия для администраторов
+            if request.user.is_superuser:
+                return True
+
+            # Во всех остальных случаях возвращаем False
+            return False
+
 
 
 class AuthorGenericAPIView(GenericAPIView, RetrieveModelMixin, ListModelMixin, CreateModelMixin, UpdateModelMixin,
                            DestroyModelMixin):
     queryset = Author.objects.all()
     serializer_class = AuthorModelSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    authentication_classes = [JWTAuthentication]
+
+  # Переопределяем атрибут permission_classes для указания нашего собственного разрешения
+    "permission_classes = [CustomPermission]"
 
     def get(self, request, *args, **kwargs):
         if kwargs.get(self.lookup_field):  # если был передан id или pk
